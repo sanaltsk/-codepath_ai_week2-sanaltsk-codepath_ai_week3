f9035a88a7f3183b3c350cdeb4968f13004bccc2
Sanal Kumar <sanaltsk@gmail.com>
2024-09-24 19:07:27 -0700

    upto milestone 3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import json
+
 from dotenv import load_dotenv
 import chainlit as cl
-
+from movie_functions import get_now_playing_movies, get_showtimes,get_reviews
 load_dotenv()
 
 # Note: If switching to LangSmith, uncomment the following, and replace @observe with @traceable
@@ -19,8 +21,14 @@ gen_kwargs = {
     "max_tokens": 500
 }
 
-SYSTEM_PROMPT = """\
-You are a pirate.
+SYSTEM_PROMPT = """
+You are a helpful assistant.
+When asked about movies showtimes, you will create a message with the movie title and location formatted as 
+{ "function": "get_showtimes", "title": "movieTitle", "location": "city, state"}
+When asked about current movies playing, you will create a message formatted as 
+{ "function": "get_now_playing_movies"}
+When asked about the reviews for a movie, you will create a message formatted as
+{ "function": "get_reviews", "movie_id": "movieId" }
 """
 
 @observe
@@ -51,8 +59,34 @@ async def on_message(message: cl.Message):
     
     response_message = await generate_response(client, message_history, gen_kwargs)
 
+    try:
+        function_call = json.loads(response_message.content)
+        if "function" in function_call:
+            if function_call["function"]=="get_now_playing_movies":
+                current_movies = get_now_playing_movies()
+                message_history.append({"role": "system", "content":  f"Result of get_now_playing_movies:{current_movies}"})
+                response_message = await generate_response(client, message_history, gen_kwargs)
+            elif function_call["function"]=="get_showtimes":
+                show_times = get_showtimes(function_call["title"], function_call["location"])
+                message_history.append({"role": "system", "content":  f"Result of get_showtimes:{show_times}"})
+                response_message = await generate_response(client, message_history, gen_kwargs)
+            elif function_call["function"] == "get_reviews":
+                review = get_reviews(function_call["movie_id"])
+                message_history.append({"role": "system", "content": f"Result of get_reviews:{review}"})
+                response_message = await generate_response(client, message_history, gen_kwargs)
+    except:
+        print("error")
     message_history.append({"role": "assistant", "content": response_message.content})
     cl.user_session.set("message_history", message_history)
 
 if __name__ == "__main__":
-    cl.main()
+    from chainlit.cli import run_chainlit
+    run_chainlit(__file__)
+
+# if function_call["function"] == "get_now_playing_movies":
+#     api_response = get_now_playing_movies()
+# elif function_call["function"] == "get_showtimes":
+#     api_response = get_showtimes(function_call["title"], function_call["location"])
+# elif function_call["function"] == "get_reviews":
+#     api_response = get_reviews(function_call["movie_id"])
+# message_history.append({"role": "system", "content": f"Result of get_reviews:{api_response}"})
